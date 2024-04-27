4b043df57a132e0dabb965f47b8187e8397bf493
Boyu Jiang <89674388+Boyu-J@users.noreply.github.com>
2024-04-27 10:31:23 -0400

    Update vae.py

--- vae.py
+++ vae.py
@@ -1,20 +1,3 @@
-import pandas as pd
-from sklearn.metrics import *
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.special import factorial
-import math
-
-import tensorflow as tf
-from scipy.special import expit
-
-import os
-os.environ["KERAS_BACKEND"] = "tensorflow"
-import keras
-from keras import layers
-from keras.regularizers import l1,l2, L1L2
-
-
 def standard_vae(input_dim, intermediate_dim, latent_dim, alpha):
     
     class Sampling(layers.Layer):
